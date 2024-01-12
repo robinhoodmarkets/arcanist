049c074d1068e380820ae113d596e9f3b253e541
xiaoyuxiu <xiaoyu.xiu@robinhood.com>
2024-01-12 01:26:26 +0000

    Collect arc runtime metrics

--- scripts/secscan_scan_pre_push.sh
+++ scripts/secscan_scan_pre_push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+EVENT_FILE=$1
+
+START_TS=$(perl -MTime::HiRes=time -e 'printf "%.0f\n", time * 1000000')
 
 set +e
 
@@ -18,6 +20,13 @@ BASE_COMMIT=$(
 bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan/tools/trufflehog:scan_local -- "$REPO_PATH" "$BASE_COMMIT"
 
 EXIT_CODE=$?
+
+END_TS=$(perl -MTime::HiRes=time -e 'printf "%.0f\n", time * 1000000')
+
+if [ -e "$EVENT_FILE" ]
+then
+    printf "{\"event_name\":\"secscan\",\"event_detail\":\"run secscan\",\"event_start_ts\":%s,\"event_end_ts\":%s}\n" "$START_TS" "$END_TS" >> "$EVENT_FILE"
+fi
 
 set -e
 
