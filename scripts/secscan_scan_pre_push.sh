2dd4840b4565fc5205271a02fbbb8d1ed3ac5927
Shreyas Sriram <shreyas.sriram@robinhood.com>
2024-02-05 17:21:59 -0800

    [secscan] Update target

--- scripts/secscan_scan_pre_push.sh
+++ scripts/secscan_scan_pre_push.sh
@@ -8,18 +8,8 @@ START_TS=$(perl -MTime::HiRes=time -e 'printf "%.0f\n", time * 1000000')
 
 set +e
 
-REPO_PATH="$(git rev-parse --show-toplevel)"
-
-BASE_COMMIT=$(
-  git log \
-    --oneline \
-    --committer="phlq" \
-    --pretty="format:%H" \
-    --max-count=50 \
-    | head -1
-)
-
-bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan/tools/trufflehog:scan_local -- "$REPO_PATH" "$BASE_COMMIT"
+# run secscan cache target
+bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress //secscan/scripts:secscan_cache
 
 EXIT_CODE=$?
 
